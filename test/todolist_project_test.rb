2ed5dfe695452b0954a81f304fe9c24f777d44d7
Nick Geballe <ngeballe@gmail.com>
2017-03-31 20:28:54 -0400

    Finish sample project

--- test/todolist_project_test.rb
+++ test/todolist_project_test.rb
@@ -157,4 +157,25 @@ class TodoListTest < MiniTest::Test
     assert_equal(list.title, @list.title)
     assert_equal(list.to_s, @list.select{ |todo| todo.done? }.to_s)
   end
+
+  def test_no_initial_due_date
+    assert_nil(@todo1.due_date)
+  end
+
+  def test_set_and_get_due_date
+    due_date = Date.today + 3
+    @todo1.due_date = due_date
+    assert_equal(due_date, @todo1.due_date)
+  end
+
+  def test_to_s_with_due_date
+    @todo2.due_date = Date.new(2017, 4, 15)
+    output = <<-OUTPUT.chomp.gsub(/^\s+/, '')
+    ---- Today's Todos ----
+    [ ] Buy milk
+    [ ] Clean room (Due: Saturday April 15)
+    [ ] Go to gym
+    OUTPUT
+    assert_equal(output, @list.to_s)
+  end
 end
